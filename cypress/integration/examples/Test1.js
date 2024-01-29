eba24312112bdec5c955431d385e8d862586f7f2
Your Name <you@example.com>
2024-01-29 15:49:05 +0530

    new commit message

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,6 @@
 describe('To validate the login page', () =>{
     it('Open the url in Google search engine', () =>{
-        cy.visit('https://quickhands-admin-frontend.vercel.app/admin/login')
+        cy.visit('https://quickhands-admin-frontend.vercel.app')
 
 
          
