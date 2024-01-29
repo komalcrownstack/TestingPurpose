da114adc56990c0eb4a09db56474c41c64483309
Your Name <you@example.com>
2024-01-29 15:38:14 +0530

    second commit

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,6 @@
 describe('To validate the login page', () =>{
     it('Open the url in Google search engine', () =>{
-        cy.visit('https://www.google.com')
+        cy.visit('https://quickhands-admin-frontend.vercel.app/admin/login')
 
 
          
